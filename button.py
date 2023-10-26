7a6eb0e2ad5512999719de8b3cdc2113531697b7
dusten45 <dalimi4511@icloud.com>
2023-10-26 14:46:22 +0900

    feat: Add button image

--- button.py
+++ button.py
@@ -1,12 +1,22 @@
 from PyQt6.QtWidgets import QToolButton, QSizePolicy
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import QSize
 
 #Define button
 class Button(QToolButton):
-    def __init__(self, text, callback):
+    def __init__(self, callback, filename):
         super().__init__()
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-        self.setText(text)
         self.clicked.connect(callback)
+        self.setImage(filename)
+
+    
+    def setImage(self, filename):
+        self.setStyleSheet('border:none')
+        icon = QIcon()
+        icon.addFile(f'./image/{filename}')
+        self.setIcon(icon)
+        self.setIconSize(QSize(350, 150))
 
 
     def sizeHint(self):
